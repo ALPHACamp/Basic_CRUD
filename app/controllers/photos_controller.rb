512e157d40d671f4adb620cf4176a11ee0b59e35
xecutioner <kapil@jyaasa.com>
2017-03-02 23:47:45 +0800

    added destroy

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -28,6 +28,12 @@ class PhotosController < ApplicationController
   redirect_to photo_path(@photo)
  end
 
+ def destroy
+  @photo = Photo.find(params[:id])
+  @photo.destroy
+  redirect_to photos_url
+ end
+
  private
 
  def photo_params
