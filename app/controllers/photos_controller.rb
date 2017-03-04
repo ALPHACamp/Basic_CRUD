3177b437e62a4a1edf31b1ad672856124ffeaf51
xecutioner <kapil@jyaasa.com>
2017-03-04 19:19:29 +0800

    added validation and re-indented photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,40 +4,46 @@ class PhotosController < ApplicationController
     @photos = Photo.all
   end
 
- def new
-  @photo = Photo.new
- end
+  def new
+    @photo = Photo.new
+  end
 
- def create
-  @photo = Photo.new(photo_params)
-  @photo.save
-  redirect_to photos_url
- end
+  def create
+    @photo = Photo.new(photo_params)
+    if  @photo.save
+      redirect_to photos_url
+    else
+      render :new
+    end
+  end
 
- def show
-  @photo = Photo.find(params[:id])
- end
+  def show
+    @photo = Photo.find(params[:id])
+  end
 
- def edit
-  @photo = Photo.find(params[:id])
- end
+  def edit
+    @photo = Photo.find(params[:id])
+  end
 
- def update
-  @photo = Photo.find(params[:id])
-  @photo.update_attributes(photo_params)
-  redirect_to photo_path(@photo)
- end
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update_attributes(photo_params)
+      redirect_to photo_path(@photo)
+    else
+      render :edit
+    end
+  end
 
- def destroy
-  @photo = Photo.find(params[:id])
-  @photo.destroy
-  redirect_to photos_url
- end
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to photos_url
+  end
 
- private
+  private
 
- def photo_params
-   params.require(:photo).permit(:title, :date, :description, :file_location )
- end
+  def photo_params
+    params.require(:photo).permit(:title, :date, :description, :file_location )
+  end
 
 end
