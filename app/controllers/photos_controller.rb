719625b67577a815df56d14df5b226d2b8b410e0
xecutioner <kapil@jyaasa.com>
2017-04-03 10:12:51 +0545

    further refactored the photos controller by using before_filter callback

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+  before_filter :set_photo, :only => [:show, :edit, :update, :destroy]
 
   def index
     @photos = Photo.all
@@ -18,15 +19,12 @@ class PhotosController < ApplicationController
   end
 
   def show
-    @photo = set_photo
   end
 
   def edit
-    @photo = set_photo
   end
 
   def update
-    @photo = set_photo
     if @photo.update_attributes(photo_params)
       redirect_to photo_path(@photo)
     else
@@ -35,7 +33,6 @@ class PhotosController < ApplicationController
   end
 
   def destroy
-    @photo = set_photo
     @photo.destroy
     redirect_to photos_url
   end
